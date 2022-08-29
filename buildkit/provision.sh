348878a36970844e05514317886243a95316c6e3
Jacob Gillespie <jacobwgillespie@gmail.com>
2022-08-29 20:21:25 +0100

    Update to BuildKit 0.10.4

--- buildkit/provision.sh
+++ buildkit/provision.sh
@@ -17,7 +17,7 @@ case "$(uname -m)" in
   *) echo >&2 "error: unsupported architecture: $(uname -m)"; exit 1 ;;
 esac
 
-curl -L "https://github.com/moby/buildkit/releases/download/v0.10.3/buildkit-v0.10.3.linux-${arch}.tar.gz" | \
+curl -L "https://github.com/moby/buildkit/releases/download/v0.10.4/buildkit-v0.10.4.linux-${arch}.tar.gz" | \
   tar -xz -C /usr/bin --strip-components=1
 
 mkdir -p /etc/buildkit
