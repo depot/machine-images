ca07146485635385baddf83d0f13b3326a9e2b18
Jacob Gillespie <jacobwgillespie@gmail.com>
2022-11-25 15:32:41 +0000

    Add swap configuration

--- buildkit/provision.sh
+++ buildkit/provision.sh
@@ -9,6 +9,14 @@ yum install -y git
 systemctl disable postfix.service
 systemctl disable update-motd.service
 
+# Configure swap
+dd if=/dev/zero of=/swapfile bs=128M count=32
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+echo "/swapfile swap swap defaults 0 0" >> /etc/fstab
+echo "vm.swappiness = 0" >> /etc/sysctl.conf
+
 # Install BuildKit
 
 case "$(uname -m)" in
