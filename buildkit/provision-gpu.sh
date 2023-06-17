6084dd11d7354bb0169afb9162ca1290f154cb83
Chris Goller <goller@gmail.com>
2023-06-17 17:47:56 -0500

    feat: prefer buildkit-runc for nvidia-container-runtime
    Signed-off-by: Chris Goller <goller@gmail.com>

--- buildkit/provision-gpu.sh
+++ buildkit/provision-gpu.sh
@@ -2,7 +2,28 @@
 set -ex
 
 # Add customizations to the GPU image here.
-ln -s /usr/bin/buildkit-runc /usr/bin/runc
 
 systemctl disable docker.service
 systemctl disable docker.socket
+
+cat << EOF >  /etc/nvidia-container-runtime/config.toml
+disable-require = false
+
+[nvidia-container-cli]
+environment = []
+load-kmods = true
+ldconfig = "@/sbin/ldconfig"
+
+[nvidia-container-runtime]
+log-level = "info"
+mode = "auto"
+# DEPOT: prefer buildkit-runc over runc and docker-runc
+runtimes = [
+    "buildkit-runc",
+    "runc",
+    "docker-runc",
+]
+    [nvidia-container-runtime.modes.csv]
+    mount-spec-path = "/etc/nvidia-container-runtime/host-files-for-container.d"
+
+EOF
